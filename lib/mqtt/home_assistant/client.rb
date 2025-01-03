c25ed9822a8aa4764bdaa9c36daec274ef84417b
Cody Cutrer <cody@cutrer.us>
2025-01-03 13:04:28 -0700

    ensure MQTT::Client is available before extending it

--- lib/mqtt/home_assistant/client.rb
+++ lib/mqtt/home_assistant/client.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "mqtt/client"
+
 module MQTT
   module HomeAssistant
     module Client
--- lib/mqtt/home_assistant/version.rb
+++ lib/mqtt/home_assistant/version.rb
@@ -2,6 +2,6 @@
 
 module MQTT
   module HomeAssistant
-    VERSION = "1.0.1"
+    VERSION = "1.0.2"
   end
 end
